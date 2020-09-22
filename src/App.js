316091e78cf6ad4ca39ee864710f28ce6adcff4d
Fede Lemaire <fede.lemaire@gmail.com>
2020-09-22 10:15:41 -0300

    Last commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Search from './components/layouts/users/Search';
 import Alert from './components/layouts/Alert';
 import About from './components/pages/About';
+import GithubState from './context/github/GithubState';
 
 
 const App = () => {
@@ -57,40 +58,42 @@ const App = () => {
 
 
   return (
-    <Router>
-      <div className="App" >
-        <Navbar title='Github Finder' icon='fab fa-github' />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search
-                  searchUsers={searchUsers}
-                  clearUsers={clearUsers}
-                  showClear={users.length > 0 ? true : false}
-                  alerts={alerts}
-                />
-                <Users
-                  loading={loading}
-                  users={users}
-                />
-              </Fragment>
-            )} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/user/:login' render={props => (
-              <User
-                {...props}
-                getUser={getUser}
-                getUserRepos={getUserRepos}
-                user={user}
-                repos={repos}
-                loading={loading} />
-            )} />
-          </Switch>
+    <GithubState>
+      <Router>
+        <div className="App" >
+          <Navbar title='Github Finder' icon='fab fa-github' />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    alerts={alerts}
+                  />
+                  <Users
+                    loading={loading}
+                    users={users}
+                  />
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User
+                  {...props}
+                  getUser={getUser}
+                  getUserRepos={getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading} />
+              )} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GithubState>
   );
 }
 
